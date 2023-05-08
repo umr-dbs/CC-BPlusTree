2d3833fa183e9f1c4e9f669466fc2e7bdfcf1a90
Amir El-Shaikh <elshaikh@mathematik.uni-marburg.de>
2023-05-08 23:27:43 +0200

    Added update test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Concurrency Control CRUD B+Tree
-    Build:             - 07.05.2023
-    Version:           - 0.0.91 (Beta)
+    Build:             - 08.05.2023
+    Version:           - 0.0.92 (Beta)
     Enable OLC-HLE:    - cargo build --features hardware-lock-elision
     Architecture:      - x86
     OS:                - Linux, Windows
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crate::tree::bplus_tree;
 use crate::crud_model::crud_api::CRUDDispatcher;
 use crate::crud_model::crud_operation::CRUDOperation;
 use crate::crud_model::crud_operation_result::CRUDOperationResult;
-use crate::test::{beast_test, BSZ_BASE, FAN_OUT, format_insertions, gen_rand_data, hle, INDEX, Key, log_debug, MAKE_INDEX, NUM_RECORDS, S_INSERTIONS, S_STRATEGIES, S_THREADS_CPU};
+use crate::test::{beast_test, BSZ_BASE, FAN_OUT, format_insertions, gen_rand_data, hle, INDEX, Key, log_debug, MAKE_INDEX, NUM_RECORDS, Payload, S_INSERTIONS, S_STRATEGIES, S_THREADS_CPU};
 use crate::utils::smart_cell::ENABLE_YIELD;
 
 mod block;
@@ -22,8 +22,11 @@ mod test;
 fn main() {
     make_splash();
 
-    println!("Number Insertions,Number Threads,Locking Strategy,Create Time,Fan Out,Leaf Records,Block Size,Scan Time");
+    // create_scan
+    // println!("Number Insertions,Number Threads,Locking Strategy,Create Time,Fan Out,Leaf Records,Block Size,Scan Time");
 
+    // update
+    println!("Number Insertions,Number Threads,Locking Strategy,Update Time,Fan Out,Leaf Records,Block Size");
     for n in S_INSERTIONS {
         let file_suffix = format_insertions(n as _);
         let create_file = format!("create_{}.bin", file_suffix);
@@ -61,7 +64,65 @@ fn main() {
             (t1s, scans)
         };
 
-        create_scan_test(create, scan);
+        update_test(create, scan);
+        // create_scan_test(create, scan);
+    }
+}
+
+fn update_test(t1s: Vec<Key>, updates: Vec<Key>) {
+    let threads_cpu
+        = S_THREADS_CPU.to_vec();
+
+    let strategies
+        = S_STRATEGIES.to_vec();
+
+    for num_threads in threads_cpu.iter() {
+        for ls in strategies.iter() {
+            print!("{}", t1s.len());
+            print!(",{}", *num_threads);
+
+            let (_, index) = beast_test(
+                *num_threads,
+                MAKE_INDEX(ls.clone()),
+                t1s.as_slice(), true);
+
+            let chunk_size = updates.len() / *num_threads;
+            let mut slices = (0..*num_threads).map(|i| unsafe {
+                std::slice::from_raw_parts(
+                    updates.as_ptr().add(i * chunk_size),
+                    chunk_size)
+            }).collect::<VecDeque<_>>();
+
+            let index: &'static INDEX = unsafe { mem::transmute(&index) };
+
+            let start = SystemTime::now();
+            let update_handles = (0..*num_threads).map(|_| {
+                let chunk
+                    = slices.pop_front().unwrap();
+
+                thread::spawn(move ||
+                    for key in chunk {
+                        match index.dispatch(CRUDOperation::Update(*key, Payload::default())) {
+                            CRUDOperationResult::Updated(..) => {}
+                            CRUDOperationResult::Error => log_debug(format!("Not found key = {}", key)),
+                            cor =>
+                                log_debug(format!("sleepy joe hit me -> {}", cor))
+                        }})
+            }).collect::<Vec<_>>();
+
+            update_handles
+                .into_iter()
+                .for_each(|handle|
+                    handle.join().unwrap());
+
+            let update_time
+                = SystemTime::now().duration_since(start).unwrap().as_millis();
+
+            print!(",{}", update_time);
+            print!(",{}", FAN_OUT);
+            print!(",{}", NUM_RECORDS);
+            println!(",{}", BSZ_BASE);
+        }
     }
 }
 
